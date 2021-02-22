af790f62dcf221f5ace80256ee56b82cdab7996d
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-22 11:57:02 +0530

    [vijay]first commit

---
+++ MoodAnalyserDay11/AnalyseMood.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserDay11
+{
+   
+
+    public class AnalyseMood
+    {
+        string message;
+       public AnalyseMood ( string message)
+        {
+            this.message = message;
+        }
+        public void MoodAnalyse()
+        {
+            if (this.message.Contains("happy"))
+                Console.WriteLine("Happy");
+            else
+                Console.WriteLine("sad");
+
+        }
+        
+        
+    }
+}
---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        string message;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
